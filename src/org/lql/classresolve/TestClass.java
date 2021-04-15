4f753872e70a07ff0d5d66f0949af00791cd2268
leiqlxk <472990991@qq.com>
2021-04-15 16:51:30 +0800

    工具测试

---
+++ src/org/lql/classresolve/TestClass.java
@@ -0,0 +1,18 @@
+package org.lql.classresolve;
+
+/**
+ * Title: TestClass <br>
+ * ProjectName: jvmTest <br>
+ * description: 字节码分析 <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/4/15 10:51 <br>
+ */
+public class TestClass {
+    private int m;
+
+    public int inc() {
+        return m + 1;
+    }
+}
